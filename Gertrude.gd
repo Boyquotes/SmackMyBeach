8ed6300188000551f078b972ad7d19adb9cca9e1
Boyquotes <webmaster@linux-live-cd.org>
2018-08-11 02:08:21 +0200

    Ecran Titre accueil du jeu + Gertrude comme ennemi

---
+++ Gertrude.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export (int) var MIN_SPEED
+export (int) var MAX_SPEED
+
+func _on_Visibility_screen_exited():
+	queue_free()
---
+++ Titre.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _input(event):
+	get_tree().change_scene("res://Beach.tscn")
+	
