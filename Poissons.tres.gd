71646e238bb53b3d9c61e2fdce557885c079652d
romsy <romse31@gmail.com>
2018-08-12 16:04:35 +0200

    reset

---
+++ Poissons.tres.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export (int) var MAXSPEED
+export (int) var MINSPEED
+export (int) var MOVESPEED = 60
+
+var target= Vector2()
+
+
+func _ready():
+	
+	
+	
+	pass
+
+#func _process(delta):
