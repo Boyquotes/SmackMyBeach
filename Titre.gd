bc444ace8d74699fcab118f9b945e840e95b2847
François Girault <francois.girault@scalian.com>
2018-08-12 23:01:05 +0200

    UX: suggestion des actions

--- Titre.gd
+++ Titre.gd
@@ -3,3 +3,7 @@ extends Node2D
 func _input(event):
 	if event.is_action("kick"):
 		get_tree().change_scene("res://Beach.tscn")
+
+
+func _on_Tween_tween_step(object, key, elapsed, value):
+	pass # replace with function body
