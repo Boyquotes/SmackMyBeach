823482d062b26d9600fa343bd9e6d9e01a91dcee
Boyquotes <webmaster@linux-live-cd.org>
2018-08-12 02:47:13 +0200

    Retrait code inutile et correction du design de la baigneuse

--- Beach.gd
+++ Beach.gd
@@ -26,60 +26,13 @@ func _ready():
 
 func _process(delta):
 	time_score =  floor((OS.get_ticks_msec() - initial_time) / 50) * 10
-	$"header/Label".text = "Score: " + str(time_score) 
-	
+	$"header/Label".text = "Score: " + str(time_score)
 	$"header/stress".value = $"YSort/Player".stressval * 100
 
-func moins_de_place():
-	score -= 1
-	#get_node("HUD/Vie").text = str(score)
-	
-
 func _on_GertrudeTimer_timeout():
-	
 	var array = [LaMouette, Gertrude]
 	#La femme maillot rouge
 	var gertrude = array[randi() % 2].instance()
 	$"GertrudePath/GertrudeSpawnLocation".unit_offset = randf()
 	gertrude.position = $"GertrudePath/GertrudeSpawnLocation".global_position
 	$YSort.add_child(gertrude)
-	
-	count += 1
-	if count == 5:
-		moins_de_place()
-		count = 0
-	# Set the LaMouette's direction perpendicular to the path direction.
-	#for i in range(0,400):
-		#lamouette.position = Vector2(i,100)
-	
-	#lamouette.position = Vector2(400,10)
-	# Set the LaMouette's position to a random location.
-	#lamouette.move_and_slide($GertrudePath/GertrudeSpawnLocation.position)
-	# Add some randomness to the direction.
-
-	# lamouette.rotation = direction
-	# Choose the velocity.
-#func _on_Beach_emplacement():
-#	print("prends l'emplacement")
-#	#emit_signal("stopLaMouette")
-#	pass # replace with function body
-#
-#
-#func _on_Beach_stopLaMouette():
-#	#$LaMouette.alors()
-#	print("_on_stopLaMouette")
-#	pass # replace with function body
-
-func _on_Emplacement_area_entered(area):
-	if area.is_in_group('Player'):
-		print('le joeur est la')
-	if !area.is_in_group("Player"):
-		var position_emplacement = area.position
-	#	print("111")
-	#	print(area)
-	#	print(emplacement_libre)
-		if emplacement_libre:
-			area.lezarde(position_emplacement)
-			moins_de_place()
-			emplacement_libre = false
-	pass # replace with function body
